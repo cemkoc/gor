4290f289a68aa68fa3895df752b7b54bb0caf08a
Cem Koc <cemkoc95@gmail.com>
2015-08-06 18:19:45 -0500

    implementing regex find on placements

--- response_interceptor.go
+++ response_interceptor.go
@@ -4,13 +4,12 @@ import (
 	//"io"
 	//"io/ioutil"
 	"log"
-	//"sync/atomic"
-	//"time"
 	"net/http"
-	//"net/url"
-	//"regexp"
-	//"fmt"
+	"regexp"
 )
+var TOTAL_AUCTION_NUMBER int = 0
+var TARGETED_AUCTIONS_NUMBER int = 0
+var BIDDED_AUCTIONS_NUMBER int = 0
 
 type ResponseInterceptor struct {
 	//Dummy struct don't delete
@@ -18,7 +17,12 @@ type ResponseInterceptor struct {
 
 func CalculateStatistics(req *http.Request, resp *http.Response) bool {
 	// Implement the statistical sampling here
+	replayed_placement := req.Header.Get("Placement-Id")
+	competing_placements_list := resp.Header.Get("All-Competing-Placement-Ids")
+	bidding_placement := resp.Header.Get("Winning-Placement-Id")
+	
 		
+
 	
 	return false
 }
@@ -28,11 +32,11 @@ func (respInter *ResponseInterceptor) ResponseAnalyze(req *http.Request, resp *h
 		// nil http response
 		return
 	}
-
+	
 	log.Println("Replaying auctions for this placement: " + req.Header.Get("Placement-Id"))
         log.Println("Targeted Placements: " + resp.Header.Get("All-Competing-Placement-Ids"))
         log.Println("Bidding Placement: " + resp.Header.Get("Winning-Placement-Id"))
-
+	
 	//isEnough = calculateStatistics()
 	//if isEnough == true {
 		//kill the GOR Replay process
