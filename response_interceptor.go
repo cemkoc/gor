d909dfa5fa23796fa644aab0dab75f49ca83d51a
Cem Koc <cemkoc95@gmail.com>
2015-08-07 17:47:10 -0500

    output to a file before killing

--- response_interceptor.go
+++ response_interceptor.go
@@ -20,31 +20,45 @@ type ResponseInterceptor struct {
 func CalculateStatistics(req *http.Request, resp *http.Response) {
 	// Implement the statistical sampling here
 	
-	if time.Now().After(KILL_TIME) {
-		log.Println("Killing the process due to overtime")
-		os.Exit(0)
-	}
 	TOTAL_AUCTION_NUMBER++
 	replayed_placement := req.Header.Get("Placement-Id")
 	competing_placements_list := resp.Header.Get("All-Competing-Placement-Ids")
 	bidding_placement := resp.Header.Get("Winning-Placement-Id")
-	
-	re := regexp.MustCompile("\\b" + replayed_placement + "\\b")
 
+	re := regexp.MustCompile("\\b" + replayed_placement + "\\b")
 	if len(re.FindAllString(competing_placements_list, -1)) >= 1 {
 		TARGETED_AUCTIONS_NUMBER++
 	}
 	if len(re.FindAllString(bidding_placement, -1)) >= 1 {
 		BIDDED_AUCTIONS_NUMBER++
 	}
-		
 	targeted_ratio_sofar := float64(TARGETED_AUCTIONS_NUMBER)/float64(TOTAL_AUCTION_NUMBER) * 100
-	bidded_ratio_sofar := float64(BIDDED_AUCTIONS_NUMBER)/float64(TOTAL_AUCTION_NUMBER) * 100
+	bidded_ratio_sofar := float64(BIDDED_AUCTIONS_NUMBER)/float64(TOTAL_AUCTION_NUMBER) * 100	
 
 	log.Println("Total number of auctions: " + strconv.Itoa(TOTAL_AUCTION_NUMBER))
-	log.Println("Targeted Ratio for placement " + replayed_placement + " is: "+ strconv.FormatFloat(targeted_ratio_sofar, 'f', 3, 64) + " percent")
-	log.Println("Bidded Ratio for placement " + replayed_placement + " is: "+ strconv.FormatFloat(bidded_ratio_sofar, 'f', 3, 64) + " percent")
+        log.Println("Targeted Ratio for placement " + replayed_placement + " is: "+ strconv.FormatFloat(targeted_ratio_sofar, 'f', 3, 64) + " percent")
+        log.Println("Bidded Ratio for placement " + replayed_placement + " is: "+ strconv.FormatFloat(bidded_ratio_sofar, 'f', 3, 64) + " percent")
 
+	if time.Now().After(KILL_TIME) {
+		log.Println("Writing to file before killing the process")
+		outfile, err := os.Create("placement_stats.txt")
+		if err != nil {
+			panic(err)
+		}
+		if _, err := outfile.WriteString("Total number of auctions: " + strconv.Itoa(TOTAL_AUCTION_NUMBER) + "\n"); err != nil {
+			panic(err)
+		}
+		if _, err := outfile.WriteString("Targeted Ratio for placement " + replayed_placement + " is: "+ strconv.FormatFloat(targeted_ratio_sofar, 'f', 3, 64) + " percent\n"); err != nil {
+			panic(err)
+		}
+		if _, err := outfile.WriteString("Bidded Ratio for placement " + replayed_placement + " is: "+ strconv.FormatFloat(bidded_ratio_sofar, 'f', 3, 64) + " percent\n"); err != nil {
+			panic(err)
+		}
+
+		outfile.Close()
+		log.Println("Killing the process due to overtime")
+		os.Exit(0)
+	}
 }
 
 func (respInter *ResponseInterceptor) ResponseAnalyze(req *http.Request, resp *http.Response) {
@@ -53,15 +67,5 @@ func (respInter *ResponseInterceptor) ResponseAnalyze(req *http.Request, resp *h
 		return
 	}
 	
-	log.Println("Replaying auctions for this placement: " + req.Header.Get("Placement-Id"))
-        log.Println("Targeted Placements: " + resp.Header.Get("All-Competing-Placement-Ids"))
-        log.Println("Bidding Placement: " + resp.Header.Get("Winning-Placement-Id"))
-
 	CalculateStatistics(req, resp)
-	
-	//isEnough = calculateStatistics()
-	//if isEnough == true {
-		//kill the GOR Replay process
-	//}
-
 }
