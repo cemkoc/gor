eb9932788396738d6a6af99555747beca1e9fe23
Cem Koc <cemkoc95@gmail.com>
2015-08-06 19:38:08 -0500

    added regex search

--- response_interceptor.go
+++ response_interceptor.go
@@ -15,16 +15,28 @@ type ResponseInterceptor struct {
 	//Dummy struct don't delete
 }
 
-func CalculateStatistics(req *http.Request, resp *http.Response) bool {
+func CalculateStatistics(req *http.Request, resp *http.Response) {
 	// Implement the statistical sampling here
+	TOTAL_AUCTION_NUMBER++
 	replayed_placement := req.Header.Get("Placement-Id")
 	competing_placements_list := resp.Header.Get("All-Competing-Placement-Ids")
 	bidding_placement := resp.Header.Get("Winning-Placement-Id")
 	
-		
+	re := regexp.MustCompile("\\b" + replayed_placement + "\\b")
 
+	if len(re.FindAllString(competing_placements_list, -1)) >= 1 {
+		TARGETED_AUCTIONS_NUMBER++
+	}
+	if len(re.FindAllString(bidding_placement, -1)) >= 1 {
+		BIDDED_AUCTIONS_NUMBER++
+	}
+		
+	targeted_ratio_sofar := float64(TARGETED_AUCTIONS_NUMBER)/float64(TOTAL_AUCTION_NUMBER)
+	bidded_ratio_sofar := float64(BIDDED_AUCTIONS_NUMBER)/float64(TOTAL_AUCTION_NUMBER)
 	
-	return false
+	log.Println("Targeted Ratio for placement " + replayed_placement + " is: %d percent", targeted_ratio_sofar)
+	log.Println("Bidded Ratio for placement " + replayed_placement + " is: %d percent", bidded_ratio_sofar)
+	 
 }
 
 func (respInter *ResponseInterceptor) ResponseAnalyze(req *http.Request, resp *http.Response) {
