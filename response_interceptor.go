c41ac335ebd4c3b1ce5e075cc39302336dc59066
Cem Koc <cemkoc95@gmail.com>
2015-08-20 05:20:29 -0500

    scaled counting logic

--- response_interceptor.go
+++ response_interceptor.go
@@ -9,8 +9,8 @@ import (
 	"time"
 )
 var TOTAL_AUCTION_NUMBER int = 0
-var TARGETED_AUCTIONS_NUMBER int = 0
-var BIDDED_AUCTIONS_NUMBER int = 0
+var TARGETED_AUCTIONS_NUMBER int = 1
+var BIDDED_AUCTIONS_NUMBER int = 1
 var KILL_TIME = time.Now().Add(time.Second*5)
 
 type ResponseInterceptor struct {
@@ -32,8 +32,8 @@ func CalculateStatistics(req *http.Request, resp *http.Response) {
 	if len(re.FindAllString(bidding_placement, -1)) >= 1 {
 		BIDDED_AUCTIONS_NUMBER++
 	}
-	//targeted_ratio_sofar := float64(TARGETED_AUCTIONS_NUMBER)/float64(TOTAL_AUCTION_NUMBER) * 100
-	//bidded_ratio_sofar := float64(BIDDED_AUCTIONS_NUMBER)/float64(TOTAL_AUCTION_NUMBER) * 100	
+	targeted_ratio_sofar := float64(TARGETED_AUCTIONS_NUMBER)/float64(TOTAL_AUCTION_NUMBER) * 100
+	bidded_ratio_sofar := float64(BIDDED_AUCTIONS_NUMBER)/float64(TOTAL_AUCTION_NUMBER) * 100	
 
 	log.Println("Total number of auctions: " + strconv.Itoa(TOTAL_AUCTION_NUMBER))
         log.Println("Targeted Ratio for placement " + replayed_placement + " is: "+ strconv.Itoa(TARGETED_AUCTIONS_NUMBER))
@@ -49,16 +49,29 @@ func CalculateStatistics(req *http.Request, resp *http.Response) {
 		if _, err := outfile.WriteString("Total number of auctions: " + strconv.Itoa(TOTAL_AUCTION_NUMBER) + "\n"); err != nil {
 			panic(err)
 		}
-		if _, err := outfile.WriteString("Targeted Ratio for placement " + replayed_placement + " is: "+ strconv.Itoa(TARGETED_AUCTIONS_NUMBER) + "\n"); err != nil {
+		if _, err := outfile.WriteString("Targeted Auctions for placement " + replayed_placement + " is: "+ strconv.Itoa(TARGETED_AUCTIONS_NUMBER) + "\n"); err != nil {
 			panic(err)
 		}
-		if _,err := outfile.WriteString("Bidded Ratio for placement " + replayed_placement + " is: "+ strconv.Itoa(BIDDED_AUCTIONS_NUMBER) + "\n"); err != nil {
+		if _,err := outfile.WriteString("Bidded Auctions for placement " + replayed_placement + " is: "+ strconv.Itoa(BIDDED_AUCTIONS_NUMBER) + "\n"); err != nil {
 		    panic(err)
 		}
-		//if _, err := outfile.WriteString("Bidded Ratio for placement " + replayed_placement + " is: "+ strconv.FormatFloat(bidded_ratio_sofar, 'f', 3, 64) + " percent\n"); err != nil {
-		//	panic(err)
-		//}
-
+		if _,err := outfile.WriteString("Replay Completion Timestamp:  " + time.Now().Format("20060102150405") + "\n"); err != nil {
+		        panic(err)
+		}
+		DAILY_AUCTIONS_TOTAL := float64(45000000000)
+                SCALED_TARGETED := DAILY_AUCTIONS_TOTAL * targeted_ratio_sofar
+                SCALED_BIDDING := DAILY_AUCTIONS_TOTAL * bidded_ratio_sofar
+		
+		if _, err := outfile.WriteString("Daily Scaled Total number of auctions: " + strconv.FormatFloat(DAILY_AUCTIONS_TOTAL, 'f', 0, 64) + "\n"); err != nil {
+			panic(err)
+		}
+		if _, err := outfile.WriteString("Daily Scaled Targeted number of auctions: " + strconv.FormatFloat(SCALED_TARGETED, 'f', 0, 64) + "\n"); err != nil {
+			panic(err)
+		}
+		if _, err := outfile.WriteString("Daily Scaled Bidded number of auctions: " + strconv.FormatFloat(SCALED_BIDDING, 'f', 0, 64) + "\n"); err != nil {
+			panic(err)
+		}
+	
 		outfile.Close()
 		log.Println("Killing the process due to overtime")
 		
